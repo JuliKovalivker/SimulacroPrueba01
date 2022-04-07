c65c8e47fba3b30ee03483b37740c8323bb13486
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2022-04-07 10:09:26 -0300

    simulacro terminado

---
+++ Assets/Scripts/EJ01.cs
@@ -0,0 +1,23 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ01 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log(num1 + " y " + num2 + " son iguales");
+        }
+        else
+        {
+            Debug.Log(num1 + " y " + num2 + " no son iguales");
+        }
+    }
+
+}//class
---
+++ Assets/Scripts/EJ02.cs
@@ -0,0 +1,33 @@
+﻿//2. Crear las variables necesarias para ingresar por Inspector
+//el precio de 3 productos y un monto de dinero disponible.
+//Comunicar si la suma de los mismos supera o no el monto de 
+//dinero disponible.Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ02 : MonoBehaviour
+{
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int dineroDisponible;
+
+    void Start()
+    {
+        int total = precio1 + precio2 + precio3;
+        if (total > dineroDisponible)
+        {
+            Debug.Log("No alcanza el dinero para comprar los tres productos, faltan $" + (total - dineroDisponible));
+        }
+        else if (total < dineroDisponible)
+        {
+            Debug.Log("El  dinero sobra para comprar los tres productos, sobran $" + (dineroDisponible - total));
+        }
+        else if (total == dineroDisponible)
+        {
+            Debug.Log("El dinero alcanza justo para comprar los tres productos, no sobra ni falta plata");
+        }
+    }
+
+}//class
